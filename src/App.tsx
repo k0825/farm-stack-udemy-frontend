5749b1a3d46688d73a38fa333724e9b5390a6059
k0825 <kazukkiyo@icloud.com>
2022-11-24 15:30:59 +0000

    ファイルの整理

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,11 @@ import { useEffect } from 'react';
 import { selectCsrfState } from './slices/appSlices';
 import { CsrfToken } from './types/types';
 import { useAppSelector } from './app/hooks';
+import { getCsrfToken } from './hooks/csrf';
 
-const App = () => {
+export const App = () => {
   const csrf = useAppSelector(selectCsrfState);
-
-  useEffect(() => {
-    const getCsrfToken = async () => {
-      const res = await axios.get<CsrfToken>(
-        `${process.env.REACT_APP_API_URL}/csrftoken`
-      );
-      axios.defaults.headers.common['X-CSRF-Token'] = res.data.csrf_token;
-      console.log(res.data.csrf_token);
-    };
-    getCsrfToken();
-  }, [csrf]);
+  getCsrfToken(csrf);
 
   return <div></div>;
 };
-
-export default App;
---
+++ src/hooks/csrf.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { CsrfToken } from '../types/types';
+
+export const getCsrfToken = (csrf: boolean) => {
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get<CsrfToken>(
+        `${process.env.REACT_APP_API_URL}/csrftoken`
+      );
+      axios.defaults.headers.common['X-CSRF-Token'] = res.data.csrf_token;
+      console.log(res.data.csrf_token);
+    })();
+  }, [csrf]);
+};
