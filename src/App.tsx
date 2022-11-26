6058b92ed14b771f5c7c2ecf4a1acc0dae04c620
k0825 <kazukkiyo@icloud.com>
2022-11-26 11:28:23 +0000

    不要なインポートを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import axios from 'axios';
-import { useEffect } from 'react';
 import { selectCsrfState } from './slices/appSlices';
-import { CsrfToken } from './types/types';
 import { useAppSelector } from './app/hooks';
 import { getCsrfToken } from './hooks/csrf';
 
