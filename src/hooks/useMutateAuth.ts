b51b19e34bf566847df7cfdf09e35a6f4396a024
k0825 <kazukkiyo@icloud.com>
2022-11-27 13:56:02 +0000

    useMutateAuth.ts

--- src/hooks/useMutateAuth.ts
+++ src/hooks/useMutateAuth.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../app/hooks';
-import { toggleCsrfState } from '../slices/appSlices';
+import { resetEditedTask, toggleCsrfState } from '../slices/appSlices';
 import { User } from '../types/types';
 
 export const useMutateAuth = () => {
@@ -11,21 +11,58 @@ export const useMutateAuth = () => {
 
   const loginMutation = useMutation(
     async (user: User) =>
-      await axios.post(`${process.env.REACT_APP_API_URL}/login`, user {
+      await axios.post(`${process.env.REACT_APP_API_URL}/login`, user, {
         withCredentials: true,
       }),
-      {
-        onSuccess: () => {
-            navigate("/todo")
-        },
-        onError: (err: any) => {
-            alert(`${err.response.data.detail}\n${err.message}`)
-            if (err.response.data.detail === 'The CSRF Token has expired.') {
-                dispatch(toggleCsrfState())
-            }
+    {
+      onSuccess: () => {
+        navigate('/todo');
+      },
+      onError: (err: any) => {
+        alert(`${err.response.data.detail}\n${err.message}`);
+        if (err.response.data.detail === 'The CSRF Token has expired.') {
+          dispatch(toggleCsrfState());
         }
-      }
+      },
+    }
   );
 
-  return {};
+  const registerMutation = useMutation(
+    async (user: User) =>
+      await axios.post(`${process.env.REACT_APP_API_URL}/register`, user),
+    {
+      onError: (err: any) => {
+        alert(`${err.response.data.detail}\n${err.message}`);
+        if (err.response.data.detail === 'The CSRF Token has expired.') {
+          dispatch(toggleCsrfState());
+        }
+      },
+    }
+  );
+
+  const logoutMutation = useMutation(
+    async () =>
+      await axios.post(
+        `${process.env.REACT_APP_API_URL}/logout`,
+        {},
+        {
+          withCredentials: true,
+        }
+      ),
+    {
+      onSuccess: () => {
+        navigate('/');
+      },
+      onError: (err: any) => {
+        alert(`${err.response.data.detail}\n${err.message}`);
+        if (err.response.data.detail === 'The CSRF Token has expired.') {
+          dispatch(toggleCsrfState());
+          dispatch(resetEditedTask());
+          navigate('/');
+        }
+      },
+    }
+  );
+
+  return { loginMutation, registerMutation, logoutMutation };
 };
