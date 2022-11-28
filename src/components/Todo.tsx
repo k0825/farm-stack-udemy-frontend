7922b4d25611834d840055a90beac527e5a4f4f4
k0825 <kazukkiyo@icloud.com>
2022-11-28 14:48:20 +0000

    ログアウト実装

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,3 +1,16 @@
+import { ArrowRightOnRectangleIcon } from '@heroicons/react/24/solid';
+import { useProcessAuth } from '../hooks/useProcessAuth';
+
 export const Todo = () => {
-  return <>Todo</>;
+  const { logout } = useProcessAuth();
+
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
+      <ArrowRightOnRectangleIcon
+        onClick={logout}
+        className="h-7 w-7 mt-1 mb-5 text-blue-500 cursor-pointer"
+      />
+      Todo
+    </div>
+  );
 };
