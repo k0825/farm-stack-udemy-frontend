3994ad15c7c6444f2c80bf9f8ff18f1330fc4b39
k0825 <kazukkiyo@icloud.com>
2022-11-29 14:02:01 +0000

    taskを取得するuseQuery関数を作成

---
+++ src/hooks/useQueryTasks.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import { Task } from '../types/types';
+
+export const useQueryTasks = () => {
+  const getTasks = async () => {
+    const { data } = await axios.get<Task[]>(
+      `${process.env.REACT_APP_API_URL}/todo`,
+      {
+        withCredentials: true,
+      }
+    );
+    return data;
+  };
+  return useQuery<Task[], Error>({
+    queryKey: 'tasks',
+    queryFn: getTasks,
+    staleTime: 5000,
+  });
+};
